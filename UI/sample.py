6962965afc0d858b53dab5bd4487ec808620b8d1
Abhiram Laha <90309476+Abhiram-Laha@users.noreply.github.com>
2023-03-26 20:58:23 +0530

    Add files via upload

---
+++ UI/sample.py
@@ -0,0 +1,9 @@
+
+
+
+
+celb = {"Aamir_Khan": 0, "Aishwarya_Rai": 1, "Ajay_Devgn": 2, "Akshay_Kumar": 3, "Amitabh_Bachchan": 4, "Anil_Kapoor": 5, "Bobby_Deol": 6, "Deepika_Padukone": 7, "Hrithik_Roshan": 8, "John_Abraham": 9, "Kareena_Kapoor": 10, "Kartik_Aaryan": 11, "Katrina_Kaif": 12, "Kriti_Sanon": 13, "Madhuri_Dixit": 14, "Manoj_Bajpayee": 15, "Priyanka_Chopra": 16, "Ranveer_Singh": 17, "Salman_Khan": 18, "Sanjay_Dutt": 19, "Shahid_Kapoor": 20, "Shah_Rukh_Khan": 21, "Shraddha_Kapoor": 22, "Tiger_Shroff": 23, "Varun_Dhawan": 24, "Vicky_Kaushal": 25, "Vidya_Balan": 26, "Vivek_Oberoi": 27, "Yami_Gautam": 28, "Zareen_Khan": 29}
+a = 1
+for i  in celb.keys():
+    print(a,". ",i)
+    a = a+1
